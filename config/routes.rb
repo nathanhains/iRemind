e0005440958dc4e72e67c358f9770b5b1f0c8c7a
nathan hains <nathanhains14@yahoo.com>
2021-05-23 11:58:33 -0400

    List controller and routes for index and create.

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,10 @@ Rails.application.routes.draw do
       resources :reminders, only: [:index, :create]
     end
   end
+
+  namespace :api do
+    namespace :v1 do
+      resources :lists, only: [:index, :create]
+    end
+  end
 end
