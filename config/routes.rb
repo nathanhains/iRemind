4d84037cc0d9dc3dd27f5ea4fd3e6ced44b0a03d
nathan hains <nathanhains14@yahoo.com>
2021-05-25 18:59:41 -0400

    List update implementation.

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :lists, only: [:index, :create]
+      resources :lists, only: [:index, :create, :update]
     end
   end
 end
